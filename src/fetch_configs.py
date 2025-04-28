69f58336e6601ed5879bcc2a3d1cd5fcef77e662
redfree8 <pooriaredfree8@gmail.com>
2025-04-28 17:43:04 +0330

    Update fetch_configs.py

--- src/fetch_configs.py
+++ src/fetch_configs.py
@@ -303,7 +303,7 @@ def save_configs(configs: List[str], config: ProxyConfig):
     try:
         os.makedirs(os.path.dirname(config.OUTPUT_FILE), exist_ok=True)
         with open(config.OUTPUT_FILE, 'w', encoding='utf-8') as f:
-            header = """//profile-title: base64:8J+lt0BSRURGUkVFOA==
+            header = """//profile-title: base64:8J+lt1NzQFJFREZSRUU4
 //profile-update-interval: 1
 //subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
 //support-url: https://t.me/redfree8bot
